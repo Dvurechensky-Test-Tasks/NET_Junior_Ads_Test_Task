932a1cf63b0b8d020cfffa32d73360a567200a0b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-20 14:13:47 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-20

--- AdRegionService/AdRegionService.Tests/Integration/AdPlatformApiIntegrationTests.cs
+++ AdRegionService/AdRegionService.Tests/Integration/AdPlatformApiIntegrationTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 сентября 2025 06:50:21
- * Version: 1.0.43
+ * Last Updated: 20 сентября 2025 14:13:46
+ * Version: 1.0.44
  */
 
 using System.Net.Http.Headers;

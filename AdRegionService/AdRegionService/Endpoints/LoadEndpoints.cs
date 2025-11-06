3c762c08c7294e4631302c7131bb51ab713c47fd
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-06 06:50:22 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-06

--- AdRegionService/AdRegionService/Endpoints/LoadEndpoints.cs
+++ AdRegionService/AdRegionService/Endpoints/LoadEndpoints.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 ноября 2025 06:50:20
- * Version: 1.0.91
+ * Last Updated: 06 ноября 2025 06:50:20
+ * Version: 1.0.92
  */
 
 using AdRegionService.Services;
